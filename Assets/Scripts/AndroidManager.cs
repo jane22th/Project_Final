36683e0c54715b4fc2883a1384ee97d186bee894
jane22th <jane22th@jane22thui-MacBook-Pro.local>
2016-01-08 01:01:47 +0900

    fail...

---
+++ Assets/Scripts/AndroidManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidManager : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		if(Application.platform == RuntimePlatform.Android)
+		{
+			if(Input.GetKey(KeyCode.Escape))
+			{
+				// 할꺼 하셈
+				 Application.Quit();
+			}
+		}
+	}
+}
