107b4f15a99e941763f065def25abe6d06137aa7
jane22th <jane22th@jane22thui-MacBook-Pro.local>
2016-01-08 22:02:16 +0900

    2016/01/08 + 기본 ui/ 화면 분할/prefab

---
+++ Assets/Scripts/Splashback.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splashback : MonoBehaviour {
+	public GameObject linked_camera;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnMouseDown(){
+		
+		Debug.Log ("hi");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
